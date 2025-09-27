159f0220674c039b0d54349332b28e8fc9833320
Gabriela Marin <gmarin@Gabrielas-MacBook-Pro.local>
2025-09-27 08:23:43 -0500

    variable

--- api/scripts/gpxScraper.js
+++ api/scripts/gpxScraper.js
@@ -1,37 +1,35 @@
 import fs from "fs";
 import path from "path";
 
-
 export async function scrapeGPX(routeNumber) {
-    console.log("start")
+  console.log("start");
   const url = new URL(`https://www.gmap-pedometer.com/gp/ajaxRoute/get`);
-    const data = await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({ rId: "7794474"}), // todo make this routeNumber
-    })
-    let body = await data.text()
-    console.log(body)
-    let polyIndex = body.indexOf("polyline=");
-    let test2 = body.substring(polyIndex+9, body.indexOf("&", polyIndex))
-    console.log(test2)
-    let arr = test2.split('a')
-    console.log(arr)
-    let newArr = [];
-    for (let i = 0; i < arr.length -1; i+=2) {
-        newArr.push({lng: arr[i+1], lat: arr[i]})
-    }
-    console.log(newArr) //todo window.gLatLngArray seems to truncate trailing zeros which im not you can if you need to
-
+  const data = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ rId: routeNumber }),
+  });
+  let body = await data.text();
+  console.log(body);
+  let polyIndex = body.indexOf("polyline=");
+  let test2 = body.substring(polyIndex + 9, body.indexOf("&", polyIndex));
+  console.log(test2);
+  let arr = test2.split("a");
+  console.log(arr);
+  let newArr = [];
+  for (let i = 0; i < arr.length - 1; i += 2) {
+    newArr.push({ lng: arr[i + 1], lat: arr[i] });
+  }
+  console.log(newArr); //todo window.gLatLngArray seems to truncate trailing zeros which im not you can if you need to
 
   const trackpoints = newArr.map((p) => ({
-      lat: typeof p.lat === "function" ? p.lat() : p.lat,
-      lon: typeof p.lng === "function" ? p.lng() : p.lng,
-      ele: p.ele || null, // todo this does not actually exist in the latlong array????
+    lat: typeof p.lat === "function" ? p.lat() : p.lat,
+    lon: typeof p.lng === "function" ? p.lng() : p.lng,
+    ele: p.ele || null, // todo this does not actually exist in the latlong array????
   }));
-    console.log(trackpoints)
+  console.log(trackpoints);
 
   const gpx = generateGPX(routeNumber, trackpoints);
   return gpx;
